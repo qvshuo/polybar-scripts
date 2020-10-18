7e806c8c7ae35b3f43d1e2cfe046a32e15ef37a4
qvshuo <qvshuo@foxmail.com>
2020-10-19 01:43:05 +0800

    readme & file name

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # polybar-inbox-imap-python
+
 A script that shows if there are unread mails in your IMAPs inbox. 
+
+## Configuration
+
+```
+email_address = ''
+password = ''
+imap_server = ''
+```
+
+## Module
+
+```
+[module/inbox-imap-python]
+type = custom/script
+exec = path/to/check.py
+interval = 60
+```
--- check.py
+++ check.py
@@ -2,12 +2,12 @@
 
 import imaplib
 
-username = ''
+email_address = ''
 password = ''
-server = ''
+imap_server = ''
 
-obj = imaplib.IMAP4_SSL(server, 993)
-obj.login(username, password)
+obj = imaplib.IMAP4_SSL(imap_server, 993)
+obj.login(email_address, password)
 obj.select()
 
 print("",len(obj.search(None, 'unseen')[1][0].split()))
